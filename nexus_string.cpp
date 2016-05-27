6f3853ddf045ba6d752771fed66f3079ff52535c
Sviatoslav Semchyshyn <fpgamcu@gmail.com>
2016-05-27 07:24:36 +0300

    Add string split function

--- nexus_string.cpp
+++ nexus_string.cpp
@@ -1,9 +1,27 @@
 #include "nexus_string.h"
 
-std::string trim(const std::string& arg)
+using namespace std;
+
+string trim(const string& arg)
 {
     if (arg.empty()) return arg;
     size_t b = arg.find_first_not_of(" \t\n\r");
     size_t e = arg.find_last_not_of(" \t\n\r");
     return arg.substr(b, e-b+1);
+}
+
+vector<string> split(const string& str, const string& delim)
+{
+    vector<string> tokens;
+    size_t prev = 0, pos = 0;
+    do
+    {
+        pos = str.find(delim, prev);
+        if (pos == string::npos) pos = str.length();
+        string token = str.substr(prev, pos-prev);
+        if (!token.empty()) tokens.push_back(token);
+        prev = pos + delim.length();
+    }
+    while (pos < str.length() && prev < str.length());
+    return tokens;
 }
--- nexus_string.h
+++ nexus_string.h
@@ -2,7 +2,9 @@
 #define NEXUS_STRING_H_INCLUDED
 
 #include <string>
+#include <vector>
 
 std::string trim(const std::string& arg);
+std::vector<std::string> split(const std::string& str, const std::string& delim);
 
 #endif // NEXUS_STRING_H_INCLUDED
